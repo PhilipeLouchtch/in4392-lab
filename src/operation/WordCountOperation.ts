b436f4ce64f524baa35259adaa490bb78327e3e4
Philipe Louchtch - de Raadt <philipelouchtch@outlook.com>
2018-10-15 11:01:05 +0200

    fix possibly-null warning in WordCountOperation

--- src/operation/WordCountOperation.ts
+++ src/operation/WordCountOperation.ts
@@ -1,6 +1,16 @@
 class WordCountOperation {
+
+    constructor() {
+
+    }
+
     count(input: String): number {
         let words = input.match(/\S+/g);
+
+        if (!words) {
+            return 0;
+        }
+
         return words.length;
     }
 }
