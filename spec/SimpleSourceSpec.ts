bfb95b717b6acdcc2504be97b220a6c1e6269e66
Philipe Louchtch - de Raadt <philipelouchtch@outlook.com>
2018-10-16 15:15:48 +0200

    add test for simple-data-source providing data in many promises

--- spec/SimpleSourceSpec.ts
+++ spec/SimpleSourceSpec.ts
@@ -4,37 +4,65 @@ import {Message} from "../src/source/Message";
 
 describe("test 1", () => {
     let simpleSource: SimpleSource;
-    let content: Message<string,string>;
-    let nextContent: Message<string,string>;
-
     const template = "";
 
     beforeEach(() => {
         simpleSource = new SimpleSource(template);
-
-        content = simpleSource.provideContent();
-        nextContent = simpleSource.provideContent();
     });
 
     it("should return unique messages", () => {
+        const content = simpleSource.provideContent();
+        const nextContent = simpleSource.provideContent();
+
         expect(content.data).not.toEqual(nextContent.data);
     });
 
     it("should generate content with this structure", () => {
+        const content = simpleSource.provideContent();
+        const nextContent = simpleSource.provideContent();
+
         expect(content.data).toBe(`${template} ${content.identifier}`);
         expect(nextContent.data).toBe(`${template} ${nextContent.identifier}`);
     });
 
     it("should increment counter for each next content", () => {
+        const content = simpleSource.provideContent();
+        const nextContent = simpleSource.provideContent();
+
         expect(content.identifier < nextContent.identifier).toBeTruthy();
     });
 
     it("should return same values with iterator", () => {
         const freshSimpleSource = new SimpleSource(template);
 
-        const valuesNormal = [content, nextContent];
+        const valuesNormal = [simpleSource.provideContent(), simpleSource.provideContent()];
         const valuesIterator = [freshSimpleSource.next().value, freshSimpleSource.next().value];
 
         expect(valuesNormal).toEqual(valuesIterator);
     });
+
+    it("should handle massive concurrency", async () => {
+        const promises: Promise<Message<string,string>>[] = [];
+
+        const fn = resolve => {
+            setTimeout(() => resolve(simpleSource.next().value), 2000);
+        };
+
+        let grouped: number[] = [];
+        for (let i = 0; i < 100000; i++) {
+            grouped[i] = 0;
+            promises.push(new Promise(fn));
+        }
+
+        return Promise.all(promises)
+            .then(msgs => {
+                for (let msg of msgs)
+                {
+                    grouped[msg.identifier]++;
+                }
+            })
+            .then(() =>
+                expect(grouped).not.toContain(0)
+            );
+    })
 });
