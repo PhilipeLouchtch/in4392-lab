16602e2d2d10e4c0b9ebbe7096e692a0dd3b7f6b
Philipe Louchtch - de Raadt <philipelouchtch@outlook.com>
2018-10-19 14:26:31 +0200

    refactor JobRequest, add extra param

--- src/JobRequest.ts
+++ src/JobRequest.ts
@@ -1,11 +1,8 @@
 export class JobRequest {
-    private readonly _limit: number;
-
-    constructor (limit: number) {
-        this._limit = limit;
-    }
-
-    public get limit() : number {
-        return this._limit;
+    /**
+     * @param limit Max items that are to be processed as part of the job
+     * @param param Some parameter, like name of the job, or keyword... something to make it "less trivial" than just a limit
+     */
+    constructor (readonly limit: number, readonly param: string) {
     }
 }
