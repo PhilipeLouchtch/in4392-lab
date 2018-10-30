aa3fe91966bb0099156f68956163079d2e4bc41f
Tim Speelman <timspeelman@live.nl>
2018-10-30 11:20:30 +0100

    Set default dist dir

--- publish.sh
+++ publish.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
-if [[ "$1" = "" || "$2" = "" ]]
-then
-  echo "Usage: $0 <path/to/dist/directory> <function name>"
-  exit
-fi
+function run () {
 
-# $1: Path to distro folder
-# $2: Function name
-cd $1 # required, otherwise folders /dist/test will end up in .zip
-zip -X -r ./deployment.zip *    
-aws lambda update-function-code --function-name $2 --zip-file fileb://deployment.zip
-rm deployment.zip
+  if [[ "$1" = "" ]]
+  then
+    echo "Usage: $0 <function name>"
+    return 1
+  fi
+
+  if ! [[ -d "./dist/$1" ]] 
+  then
+    echo "Folder does not exist: ./dist/$1"
+    return 1
+  fi
+
+  # $1: Path to distro folder
+  # $2: Function name
+  cd ./dist/$1 # required, otherwise folders /dist/test will end up in .zip
+  zip -X -r ./deployment.zip *    
+  # aws lambda update-function-code --function-name $2 --zip-file fileb://deployment.zip
+  rm deployment.zip
+
+}
+
+run "$1"
