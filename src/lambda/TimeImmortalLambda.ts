ddf1359d4c2563414fe1d81e5d42d9def66667c8
Philipe Louchtch - de Raadt <philipelouchtch@outlook.com>
2018-10-30 16:38:31 +0100

    cleanup TimeImmortalLambda

--- src/lambda/TimeImmortalLambda.ts
+++ src/lambda/TimeImmortalLambda.ts
@@ -2,10 +2,9 @@ import {Lambda} from "./Lambda";
 import {ExecutionTime} from "../lib/ExecutionTime";
 
 /**
- * TODO:
  * The type of lambda that needs to be run perpetually. In other words, when the threshold
  * of execution time is reached, a new Lambda is spawned. The state of the current instance is
- * checkpointed and passed on the new instance. (TODO)
+ * checkpointed and passed on the new instance (TODO: the latter).
  */
 export abstract class TimeImmortalLambda extends Lambda {
     constructor(exectutionTime: ExecutionTime) {
@@ -17,6 +16,4 @@ export abstract class TimeImmortalLambda extends Lambda {
         // also take care of saving current state and passing it on to next instance of self
         // awsLabda::spawn(self);
     }
-
-
 }
