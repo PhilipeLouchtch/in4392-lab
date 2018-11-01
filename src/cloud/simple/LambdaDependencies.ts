1424f526171943f0bdc14d2730f089465d621d25
Tim Speelman <timspeelman@live.nl>
2018-11-01 16:22:30 +0100

    Add Input Validation

--- src/cloud/simple/LambdaDependencies.ts
+++ src/cloud/simple/LambdaDependencies.ts
@@ -1,15 +1,17 @@
+import { SimpleJobParams } from '../../job/SimpleJobRequest';
+
 // TODO Move to Lambda Implementation??
 export type FeedDeps = {
-    step_zero: string,
-    step_one: string
+    step_one: string,
+    JobRequest: SimpleJobParams,
 }
 
 export type OneDeps = {
     step_one: string,
-    step_two: string
+    step_two: string,
 }
 
 export type ReduceDeps = {
     step_two: string,
-    step_three: string
+    JobRequest: SimpleJobParams,
 }
