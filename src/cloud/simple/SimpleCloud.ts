9f82e2e7b7b18c9aba7292a07b399e2b7425fd6e
Tim Speelman <timspeelman@live.nl>
2018-11-01 21:26:32 +0100

    Fix wrong queue passed, and remove excess queue

--- src/cloud/simple/SimpleCloud.ts
+++ src/cloud/simple/SimpleCloud.ts
@@ -13,7 +13,6 @@ export class SimpleCloud implements Cloud {
     readonly stepZeroQueue: QueueController
     readonly stepOneQueue: QueueController
     readonly stepTwoQueue: QueueController
-    readonly stepThreeQueue: QueueController
 
     readonly feedLambda: LambdaController<FeedDeps>
     readonly stepOneLambda: LambdaController<OneDeps>
@@ -21,16 +20,15 @@ export class SimpleCloud implements Cloud {
     readonly reduceLambda: LambdaController<ReduceDeps>
 
     constructor(lambdaClient: Lambda, sqsClient: SQS, uuid: string, job: SimpleJobRequest) {
-        const queueNames = ['zero', 'one', 'two', 'three'].map(n => `step_${n}_${uuid}`)
+        const queueNames = ['zero', 'one', 'two'].map(n => `step_${n}_${uuid}`)
         this.stepZeroQueue = new QueueController(sqsClient, queueNames[0])
         this.stepOneQueue = new QueueController(sqsClient, queueNames[1])
         this.stepTwoQueue = new QueueController(sqsClient, queueNames[2])
-        this.stepThreeQueue = new QueueController(sqsClient, queueNames[3])
 
         this.feedLambda = new LambdaController<FeedDeps>(lambdaClient, `Feed`, { output_queue: queueNames[0], JobRequest: job.parameters })
-        this.stepOneLambda = new LambdaController<OneDeps>(lambdaClient, `ProcessStepOne`, { input_queue: queueNames[1], output_queue: queueNames[2], JobRequest: job.parameters })
-        this.stepTwoLambda = new LambdaController<WordCountDeps>(lambdaClient, 'WordCount', { input_queue: queueNames[2], output_queue: queueNames[3], JobRequest: job.parameters })
-        this.reduceLambda = new LambdaController<ReduceDeps>(lambdaClient, `SummingReduce`, { in_out_queue: queueNames[3], JobRequest: job.parameters })
+        this.stepOneLambda = new LambdaController<OneDeps>(lambdaClient, `ProcessStepOne`, { input_queue: queueNames[0], output_queue: queueNames[1], JobRequest: job.parameters })
+        this.stepTwoLambda = new LambdaController<WordCountDeps>(lambdaClient, 'WordCount', { input_queue: queueNames[1], output_queue: queueNames[2], JobRequest: job.parameters })
+        this.reduceLambda = new LambdaController<ReduceDeps>(lambdaClient, `SummingReduce`, { in_out_queue: queueNames[2], JobRequest: job.parameters })
     }
 
     /**
@@ -41,7 +39,6 @@ export class SimpleCloud implements Cloud {
             this.stepZeroQueue.spawn(),
             this.stepOneQueue.spawn(),
             this.stepTwoQueue.spawn(),
-            this.stepThreeQueue.spawn(),
         ])
     }
 
