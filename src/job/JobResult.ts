83bbd66aff7c821eb7ea3741c3eae835ea6e3f3f
Philipe Louchtch - de Raadt <philipelouchtch@outlook.com>
2018-11-02 12:05:52 +0100

    track start and end times of job

--- src/job/JobResult.ts
+++ src/job/JobResult.ts
@@ -1,6 +1,27 @@
 import { JobStatus } from './JobStatus';
 
-export interface JobResult<T> {
-    status: JobStatus,
-    data?: T
+export class JobResult<T> {
+
+    private constructor(
+        public readonly status: JobStatus,
+        public readonly startedOn?: number,
+        public readonly finishedOn?: number,
+        public readonly data?: T) {
+    }
+
+    public static ofNotStarted<T>(): JobResult<T> {
+        return new JobResult<T>(JobStatus.NOT_STARTED);
+    }
+
+    started(): JobResult<T> {
+        return new JobResult<T>(JobStatus.RUNNING, Date.now());
+    }
+
+    completed(data: T): JobResult<T> {
+        return new JobResult<T>(JobStatus.COMPLETED, this.startedOn, Date.now(), data);
+    }
+
+    failed() {
+        return new JobResult<T>(JobStatus.FAILED, this.startedOn, Date.now());
+    }
 }
