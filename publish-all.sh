1eff37bfda91d17536ca480fad5796232d2eca14
Tim Speelman <timspeelman@live.nl>
2018-11-07 18:44:50 +0100

    Add Publish All Script

---
+++ publish-all.sh
@@ -0,0 +1,6 @@
+./publish-lambda.sh Api; 
+./publish-lambda.sh Daemon; 
+./publish-lambda.sh ProcessStepOne; 
+./publish-lambda.sh WordCount; 
+./publish-lambda.sh SummingReduce; 
+./publish-lambda.sh Feed; 
